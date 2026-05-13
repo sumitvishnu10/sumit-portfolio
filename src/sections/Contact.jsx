d646e21fa35212618388dd95ec18078eb3b8e9a0
SumitVishnu <sumitvishnu10@gmail.com>
2026-05-13 22:43:00 +0530

    latest portfolio update

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -18,19 +18,21 @@ function Contact() {
 
       <div className="contact-links">
 
-        <a
-          href="https://github.com/sumitvishnu10"
-          target="_blank"
-        >
-          GitHub
-        </a>
+     <a
+  href="https://github.com/sumitvishnu10"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  GitHub
+</a>
 
-        <a
-          href="https://linkedin.com"
-          target="_blank"
-        >
-          LinkedIn
-        </a>
+       <a
+  href="https://www.linkedin.com/in/sumit-vishnu-m/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BA8TRB1hUTFetbtp9MPK8ow%3D%3D"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  LinkedIn
+</a>
 <a
   href="mailto:sumitvishnu10@gmail.com?subject=Portfolio Inquiry"
   className="contact-btn"
